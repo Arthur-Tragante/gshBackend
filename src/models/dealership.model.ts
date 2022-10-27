c234758ad791f3e9926933d88c10d186fbdd8d2a
Arthur-Tragante <arthur.tragante@gmail.com>
2022-10-27 08:59:38 -0300

    Update dealership.model.ts

--- src/models/dealership.model.ts
+++ src/models/dealership.model.ts
@@ -42,6 +42,101 @@ export class DealershipModel {
   dls_requirement6?: string;
 
   @Column({ default: '' })
+  dls_requirement7?: string;
+
+  @Column({ default: '' })
+  dls_requirement8?: string;
+
+  @Column({ default: '' })
+  dls_requirement9?: string;
+
+  @Column({ default: '' })
+  dls_requirement10?: string;
+
+  @Column({ default: '' })
+  dls_requirement11?: string;
+
+  @Column({ default: '' })
+  dls_requirement12?: string;
+
+  @Column({ default: '' })
+  dls_requirement13?: string;
+  @Column({ default: '' })
+  dls_requirement14?: string;
+  @Column({ default: '' })
+  dls_requirement15?: string;
+  @Column({ default: '' })
+  dls_requirement16?: string;
+  @Column({ default: '' })
+  dls_requirement17?: string;
+  @Column({ default: '' })
+  dls_requirement18?: string;
+  @Column({ default: '' })
+  dls_requirement19?: string;
+  @Column({ default: '' })
+  dls_requirement20?: string;
+  @Column({ default: '' })
+  dls_requirement21?: string;
+  @Column({ default: '' })
+  dls_requirement22?: string;
+  @Column({ default: '' })
+  dls_requirement23?: string;
+  @Column({ default: '' })
+  dls_requirement24?: string;
+  @Column({ default: '' })
+  dls_requirement25?: string;
+  @Column({ default: '' })
+  dls_requirement26?: string;
+  @Column({ default: '' })
+  dls_requirement27?: string;
+  @Column({ default: '' })
+  dls_requirement28?: string;
+  @Column({ default: '' })
+  dls_requirement29?: string;
+  @Column({ default: '' })
+  dls_requirement30?: string;
+  @Column({ default: '' })
+  dls_requirement31?: string;
+  @Column({ default: '' })
+  dls_requirement32?: string;
+  @Column({ default: '' })
+  dls_requirement33?: string;
+  @Column({ default: '' })
+  dls_requirement34?: string;
+  @Column({ default: '' })
+  dls_requirement35?: string;
+  @Column({ default: '' })
+  dls_requirement36?: string;
+  @Column({ default: '' })
+  dls_requirement37?: string;
+  @Column({ default: '' })
+  dls_requirement38?: string;
+  @Column({ default: '' })
+  dls_requirement39?: string;
+  @Column({ default: '' })
+  dls_requirement40?: string;
+  @Column({ default: '' })
+  dls_requirement41?: string;
+  @Column({ default: '' })
+  dls_requirement42?: string;
+  @Column({ default: '' })
+  dls_requirement43?: string;
+  @Column({ default: '' })
+  dls_requirement44?: string;
+  @Column({ default: '' })
+  dls_requirement45?: string;
+  @Column({ default: '' })
+  dls_requirement46?: string;
+  @Column({ default: '' })
+  dls_requirement47?: string;
+  @Column({ default: '' })
+  dls_requirement48?: string;
+  @Column({ default: '' })
+  dls_requirement49?: string;
+  @Column({ default: '' })
+  dls_requirement50?: string;
+
+  @Column({ default: '' })
   dls_requirement_visto1?: string;
 
   @Column({ default: '' })
@@ -58,4 +153,136 @@ export class DealershipModel {
 
   @Column({ default: '' })
   dls_requirement_visto6?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto7?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto8?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto9?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto10?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto11?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto12?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto13?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto14?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto15?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto16?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto17?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto18?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto19?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto20?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto21?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto22?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto23?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto24?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto25?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto26?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto27?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto28?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto29?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto30?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto31?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto32?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto33?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto34?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto35?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto36?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto37?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto38?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto39?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto40?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto41?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto42?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto43?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto44?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto45?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto46?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto47?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto48?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto49?: string;
+
+  @Column({ default: '' })
+  dls_requirement_visto50?: string;
 }
